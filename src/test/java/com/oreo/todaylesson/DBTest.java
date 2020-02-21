3a838c83298cc3c096a90396214eccf53b377c81
kzoey <k_zoey94@naver.com>
2020-02-21 16:45:31 +0900

    햄이 테스트 커미잇

--- src/test/java/com/oreo/todaylesson/DBTest.java
+++ src/test/java/com/oreo/todaylesson/DBTest.java
@@ -1,5 +1,25 @@
 package com.oreo.todaylesson;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src\\main\\webapp\\WEB-INF\\spring\\root-context.xml")
 public class DBTest {
 
+	@Autowired
+	private HikariDataSource ds;
+	
+	@Test
+	public void t1()
+	{
+		assertNotNull(ds);
+	}
 }
